c1bddb625ee6317e59aa369989459d115fa70dc8
Ravi Rai <ravi.10819011922@ipu.ac.in>
2026-05-09 13:10:18 +0530

    Update TRUSTED_ORIGINS and baseURL configuration

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -3,7 +3,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma.js"; // Make sure this path is correct for your project
 
-const TRUSTED_ORIGINS = process.env.TRUSTED_ORIGINS ? process.env.TRUSTED_ORIGINS.split(",") : [];
+// const TRUSTED_ORIGINS = process.env.TRUSTED_ORIGINS ? process.env.TRUSTED_ORIGINS.split(",") : [];
+const TRUSTED_ORIGINS = ['https://ai-site-builder-pi.vercel.app','http://localhost:5173'];
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -26,8 +27,8 @@ export const auth = betterAuth({
   },
   
   trustedOrigins: TRUSTED_ORIGINS, 
-  // baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-  baseURL: "http://localhost:5173",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  // baseURL: "http://localhost:5173",
   secret: process.env.BETTER_AUTH_SECRET!,
   
   advanced: {
